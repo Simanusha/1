9b3c681e8f91cd3be58f81e3bbc297d84142e1b7
Class5-3 <Class5-3>
2017-04-05 17:12:20 +0300

    Initial commit

---
+++ class_work.py
@@ -0,0 +1,13 @@
+#Calculates square of rectangle
+width = 42
+height = 43
+rect_square = width * height
+print("For width %d m2 and height %d m2 rectangle square is %d m2" %
+      (width, height, rect_square))
+
+# print("Square of rectangle:", rect_square)
+# print("For width", width, "m2 and height", height, "m2 rectangle square is", rect_square, "m2")
+
+
+
+
